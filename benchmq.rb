6f73b03dded21031af758975ac0ff12d62856471
pyr33x <pyrexrj@gmail.com>
2025-09-14 21:42:26 +0330

    fix: update benchmq releases based on amd64/arm64

--- benchmq.rb
+++ benchmq.rb
@@ -1,8 +1,15 @@
 class Benchmq < Formula
   desc "BenchMQ is a simple, fast, and lightweight CLI to benchmark your MQTT broker with ease"
   homepage "https://github.com/rayomqio/benchmq"
-  url "https://github.com/rayomqio/benchmq/releases/download/v1.0.3/benchmq_Darwin_x86_64.tar.gz"
-  sha256 "9c9e977c3b83b2f556844fa682d055dcf1d730eb54386c4fd5a2c5972405c656"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/rayomqio/benchmq/releases/download/v1.0.4/benchmq_darwin_arm64.tar.gz"
+    sha256 "03fbde0dc8b365973928d1223d1bd105f3b37c4a3b1882b0b672e6dce1ec7934"
+  else
+    url "https://github.com/rayomqio/benchmq/releases/download/v1.0.4/benchmq_darwin_amd64.tar.gz"
+    sha256 "e572960169c2a931f033523950053ea56eab1c2209a4a93dcbadccfc62460bc9"
+  end
+
   license "Apache-2.0"
 
   def install
